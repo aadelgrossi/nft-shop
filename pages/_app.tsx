8263f9494983bd5d8480ce4ab87353237be5e1d8
Andre Grossi <iamandregrossi@gmail.com>
2024-09-15 14:16:54 -0300

    chore: Change default chain to arb sepolia

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,12 @@ import { ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 
+import { ArbitrumSepolia } from "@thirdweb-dev/chains";
+
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "mumbai";
+const activeChain = ArbitrumSepolia;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
